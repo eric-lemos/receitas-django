3eee2feb526fd16f5b3eb64d355b4a37877a9754
Eric <eric.lemoss@gmail.com>
2022-01-27 19:52:45 -0300

    Admin, parâmetros e receitas

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:receita_id>', views.receita, name='receita')
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from app.models import Receita
 
 def index(request):
-    return HttpResponse('<h1>Receitas</h1>')
+    receitas = Receita.objects.all()
+    dados = {'receitas': receitas}
+    return render(request, 'index.html', dados)
+
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+    receita_item = {
+        'receita': receita
+    }
+    return render(request, 'receita.html', receita_item)
